ba13bf61d726926feb88af9a6c58d177d21735bc
geoffrey-chameroy <geoffrey.chameroy@gmail.com>
2017-05-15 09:00:21 +0200

    Add Material template

---
+++ src/OfficeBundle/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace OfficeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('OfficeBundle:Home:index.html.twig');
+    }
+}
